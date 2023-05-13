09c941b2598d5f31597f9f0819b4ff7be9db5ab1
Mariam Hesham <99722575+MariamH78@users.noreply.github.com>
2023-05-13 22:43:45 +0300

    Updated gps files

--- gps.c
+++ gps.c
@@ -45,3 +45,87 @@ void ftoa (double number, char* array){
 	else
 		array[10] = '\0';
 }
+position get_pose_from_filtered_data(char* gps_data, int size) {
+	char longitude[15], latitude[15];
+	int lat_start = 0, lat_end = -1, lon_start = 0, lon_end = -1, comma_counter = 0, index = 0, i;
+
+	position current_pose = {.lon_exists = 0, .lat_exists = 0};
+
+	if (size <= 0) return current_pose;
+	for (i = 0; i < size; i++) {
+		if (gps_data[i] != ',') continue;
+
+		comma_counter++;
+		switch (comma_counter) {
+				case 2:
+					lat_start = i + 1;
+					break;
+
+				case 3:
+					lat_end = i - 1;
+					break;
+
+				case 4:
+					lon_start = i + 1;
+					break;
+
+				case 5:
+					lon_end = i - 1;
+					break;
+			}
+
+	}
+
+	if (lat_end - lat_start <= 1 || lon_end - lon_start <= 1) return current_pose;
+	for (i = lat_start; i <= lat_end; i++) {
+			latitude[index++] = (char) (gps_data[i]);
+			current_pose.lat_exists = 1;
+	}
+	latitude[index++] = '\0';
+
+  get_latitude_from_array(&current_pose, latitude, index);
+
+	index = 0;
+	for (i = lon_start; i <= lon_end; i++) {
+			longitude[index++] = (char) (gps_data[i]);
+			current_pose.lon_exists = 1;
+	}
+	longitude[index++] = '\0';
+
+  get_longitude_from_array(&current_pose, longitude, index);
+
+  return current_pose;
+}
+void get_latitude_from_array(position* current_pose, char* latitude, int size){
+  int i;
+  char latitude_degrees_array[4];
+	char latitude_minutes_array[12];
+
+	for (i = 0; i < 2; i++){
+		latitude_degrees_array[i] = latitude[i];
+	}
+	latitude_degrees_array[2] = '\0';
+
+	for (i = 2; i < size; i++){
+		latitude_minutes_array[i - 2] = latitude[i];
+	}
+	latitude_minutes_array[size] = '\0';
+	current_pose->latitude = strtol(latitude_degrees_array, '\0', 10) + strtof(latitude_minutes_array, '\0') / 60.0;
+}
+void get_longitude_from_array(position* current_pose, char* longitude, int size){
+	int i;
+	char longitude_degrees_array[5];
+	char longitude_minutes_array[15];
+
+	for (i = 0; i < 3; i++){
+		longitude_degrees_array[i] = longitude[i];
+	}
+	longitude_degrees_array[3] = '\0';
+
+	for (i = 3; i < size; i++){
+		longitude_minutes_array[i - 3] = longitude[i];
+	}
+	longitude_minutes_array[size] = '\0';
+	current_pose->longitude = strtol(longitude_degrees_array, '\0', 10) + strtof(longitude_minutes_array, '\0') / 60.0;
+}
+
--- gps.h
+++ gps.h
@@ -1,5 +1,4 @@
 #include "TM4C1234.h"
-#include "UART.h"
 #include <stdlib.h>
 #define TARGET_LAT 30
 #define TARGET_LON 30
@@ -13,3 +12,7 @@ struct position{
 	char lat_exists;
 };
 typedef struct position position;
+
+void get_latitude_from_array(position* current_pose, char* latitude, int size);
+void get_longitude_from_array(position* current_pose, char* longitude, int size);
+position  get_pose_from_filtered_data(char* gps_data, int size);
